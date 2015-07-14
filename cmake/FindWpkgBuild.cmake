17447c25eead4acbd4e54922c4acb0a5192a4026
R. Douglas Barbieri <doug@dooglio.net>
2015-07-13 22:05:25 -0700

    Now wpkgs build correctly using the root-level CMakeLists.txt.
    Lots of improvements, including multiple sub-packages and support for debug and release packaging.

--- cmake/FindWpkgBuild.cmake
+++ cmake/FindWpkgBuild.cmake
@@ -30,6 +30,9 @@
 #
 include( CMakeParseArguments )
 
+set( ECHO_CMD "" CACHE STRING "Debug echo command" )
+mark_as_advanced( ECHO_CMD )
+
 set_property( GLOBAL PROPERTY USE_FOLDERS ON )
 
 if( NOT MSVC )
@@ -41,20 +44,26 @@ endif()
 
 function( ConfigureMakeProject )
 	set( options        USE_CONFIGURE_SCRIPT NOINC_DEBVERS )
-	set( oneValueArgs   PROJECT_NAME VERSION DISTFILE_PATH )
+	set( oneValueArgs   PROJECT_NAME VERSION DISTFILE_PATH BUILD_TYPE )
 	set( multiValueArgs CONFIG_ARGS DEPENDS )
 	cmake_parse_arguments( ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 	#
 	if( NOT ARG_PROJECT_NAME )
-		message( FATAL_ERROR "You must specify PROJECT_NAME!" )
+		message( FATAL_ERROR "ConfigureMakeProject(): You must specify PROJECT_NAME!" )
 	endif()
+
+	if( NOT ARG_BUILD_TYPE )
+		message( FATAL_ERROR "ConfigureMakeProject(): BUILD_TYPE required!" )
+	endif()
+
+	set( FULL_PROJECT_NAME "${ARG_PROJECT_NAME}-${ARG_BUILD_TYPE}" )
 
 	if( ARG_VERSION )
 		set( SRC_DIR   ${CMAKE_BINARY_DIR}/${ARG_PROJECT_NAME}-${ARG_VERSION}                                    )
-		set( BUILD_DIR ${CMAKE_BINARY_DIR}/${ARG_PROJECT_NAME}-${ARG_VERSION}/${ARG_PROJECT_NAME}-${ARG_VERSION} )
+		set( BUILD_DIR ${CMAKE_BINARY_DIR}/${FULL_PROJECT_NAME}-${ARG_VERSION}/${FULL_PROJECT_NAME}-${ARG_VERSION} )
 	else()
 		set( SRC_DIR   ${CMAKE_SOURCE_DIR}/${ARG_PROJECT_NAME} )
-		set( BUILD_DIR ${CMAKE_BINARY_DIR}/${ARG_PROJECT_NAME} )
+		set( BUILD_DIR ${CMAKE_BINARY_DIR}/${FULL_PROJECT_NAME} )
 	endif()
 	set( WPKG_DIST_DIR "${CMAKE_SOURCE_DIR}/dist" CACHE PATH "Destination installation folder." )
 	if( ARG_DISTFILE_PATH )
@@ -64,18 +73,18 @@ function( ConfigureMakeProject )
 	endif()
 
 	if( NOT EXISTS ${SRC_DIR} AND NOT ARG_DISTFILE_PATH )
-		message( FATAL_ERROR "No source directory '${SRC_DIR}'!" )
+		message( FATAL_ERROR "ConfigureMakeProject(): No source directory '${SRC_DIR}'!" )
 	endif()
 
 	if( NOT EXISTS ${SRC_DIR} AND ARG_DISTFILE_PATH )
-		message( STATUS "Unpacking ${ARG_PROJECT_NAME} source distribution file into local build area." )
+		message( STATUS "Unpacking ${FULL_PROJECT_NAME} source distribution file into local build area." )
 		file( MAKE_DIRECTORY ${SRC_DIR} )
 		execute_process(
-			COMMAND ${CMAKE_COMMAND} -E tar xzf ${ARG_DISTFILE_PATH}
+			COMMAND ${ECHO_CMD} ${CMAKE_COMMAND} -E tar xzf ${ARG_DISTFILE_PATH}
 			WORKING_DIRECTORY ${SRC_DIR}
 		)
 		execute_process(
-			COMMAND chmod a+x ${BUILD_DIR}/configure
+			COMMAND ${ECHO_CMD} chmod a+x ${BUILD_DIR}/configure
 			WORKING_DIRECTORY ${BUILD_DIR}
 		)
 	endif()
@@ -84,20 +93,19 @@ function( ConfigureMakeProject )
 		file( MAKE_DIRECTORY ${BUILD_DIR} )
 	endif()
 
-	#set_property( GLOBAL PROPERTY ${ARG_PROJECT_NAME}_DEPENDS_LIST ${ARG_DEPENDS} )
-	add_custom_target( ${ARG_PROJECT_NAME}-depends DEPENDS ${ARG_DEPENDS} )
-	set_property( TARGET ${ARG_PROJECT_NAME}-depends PROPERTY FOLDER ${ARG_PROJECT_NAME} )
+	add_custom_target( ${FULL_PROJECT_NAME}-depends DEPENDS ${ARG_DEPENDS} )
+	set_property( TARGET ${FULL_PROJECT_NAME}-depends PROPERTY FOLDER ${FULL_PROJECT_NAME} )
 
 	if( ARG_USE_CONFIGURE_SCRIPT )
 		set( CONFIGURE_TARGETS ${BUILD_DIR}/config.log  )
 		add_custom_command(
 			OUTPUT ${CONFIGURE_TARGETS}
-			COMMAND ${BUILD_DIR}/configure --prefix=${WPKG_DIST_DIR} ${ARG_CONFIG_ARGS}
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.log
-				2> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.err
-			DEPENDS ${ARG_PROJECT_NAME}-depends
+			COMMAND ${ECHO_CMD} ${BUILD_DIR}/configure --prefix=${WPKG_DIST_DIR} ${ARG_CONFIG_ARGS}
+				1> ${BUILD_DIR}/${FULL_PROJECT_NAME}_configure.log
+				2> ${BUILD_DIR}/${FULL_PROJECT_NAME}_configure.err
+			DEPENDS ${FULL_PROJECT_NAME}-depends
 			WORKING_DIRECTORY ${BUILD_DIR}
-			COMMENT "Running ${ARG_PROJECT_NAME} configure script..."
+			COMMENT "Running ${FULL_PROJECT_NAME} configure script..."
 			)
 	else()
 		unset( TOOLCHAIN )
@@ -108,14 +116,14 @@ function( ConfigureMakeProject )
 			${CMAKE_COMMAND}
 				-DCMAKE_INSTALL_PREFIX:PATH=${WPKG_DIST_DIR}
 				-DCMAKE_MODULE_PATH:PATH=${WPKG_DIST_DIR}/share/cmake-2.8/Modules
-				-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-				-DDIST_PATH:PATH="${WPKG_DIST_DIR}"
+				-DCMAKE_BUILD_TYPE=${ARG_BUILD_TYPE}
+				-DWPKG_ROOT:PATH="${WPKG_DIST_DIR}"
 				-G "${CMAKE_GENERATOR}"
 				${TOOLCHAIN}
 				${ARG_CONFIG_ARGS}
 				${SRC_DIR}
 	    )
-		set( CMD_FILE ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.cmd )
+		set( CMD_FILE ${BUILD_DIR}/${FULL_PROJECT_NAME}_configure.cmd )
 		file( REMOVE ${CMD_FILE} )
 		foreach( line ${COMMAND_LIST} )
 			file( APPEND ${CMD_FILE} ${line} "\n" )
@@ -124,55 +132,44 @@ function( ConfigureMakeProject )
 		set( CONFIGURE_TARGETS ${BUILD_DIR}/CMakeCache.txt  )
 		add_custom_command(
 			OUTPUT ${CONFIGURE_TARGETS}
-			COMMAND ${COMMAND_LIST}
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.log
-				2> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.err
-			DEPENDS ${ARG_PROJECT_NAME}-depends
+			COMMAND ${ECHO_CMD} ${COMMAND_LIST}
+				1> ${BUILD_DIR}/${FULL_PROJECT_NAME}_configure.log
+				2> ${BUILD_DIR}/${FULL_PROJECT_NAME}_configure.err
+			DEPENDS ${FULL_PROJECT_NAME}-depends
 			WORKING_DIRECTORY ${BUILD_DIR}
-			COMMENT "Running ${ARG_PROJECT_NAME} CMake configuration..."
+			COMMENT "Running ${FULL_PROJECT_NAME} CMake configuration..."
 			)
 	endif()
 
 	if( MSVC )
 		if( NOT (${CMAKE_MAKE_PROGRAM} STREQUAL "nmake") AND NOT (${CMAKE_MAKE_PROGRAM} STREQUAL "jom") )
-			message( FATAL_ERROR "Please use nmake/jom at this level. This won't work right trying to use devenv." )
+			message( FATAL_ERROR "ConfigureMakeProject(): Please use nmake/jom at this level. This won't work right trying to use devenv." )
 		endif()
 	endif()
 
 	set( BUILD_CMD   ${CMAKE_BUILD_TOOL} )
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}-make
-		COMMAND ${BUILD_CMD}
-			1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.log
-			2> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.err
+		${FULL_PROJECT_NAME}-make
+		COMMAND ${ECHO_CMD} ${BUILD_CMD}
+			1> ${BUILD_DIR}/${FULL_PROJECT_NAME}_make.log
+			2> ${BUILD_DIR}/${FULL_PROJECT_NAME}_make.err
 		DEPENDS ${CONFIGURE_TARGETS}
 		WORKING_DIRECTORY ${BUILD_DIR}
-		COMMENT "Building ${ARG_PROJECT_NAME}"
+		COMMENT "Building ${FULL_PROJECT_NAME}"
 		)
-	set_property( TARGET ${ARG_PROJECT_NAME}-make PROPERTY FOLDER ${ARG_PROJECT_NAME} )
+	set_property( TARGET ${FULL_PROJECT_NAME}-make PROPERTY FOLDER ${FULL_PROJECT_NAME} )
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}-install
-		COMMAND ${BUILD_CMD} install
-			1>> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.log
-			2>> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.err
-		DEPENDS ${ARG_PROJECT_NAME}-make
+		${FULL_PROJECT_NAME}-package
+		COMMAND ${ECHO_CMD} ${BUILD_CMD} package
+			1>> ${BUILD_DIR}/${FULL_PROJECT_NAME}_make.log
+			2>> ${BUILD_DIR}/${FULL_PROJECT_NAME}_make.err
+		DEPENDS ${FULL_PROJECT_NAME}-make
 		WORKING_DIRECTORY ${BUILD_DIR}
-		COMMENT "Installing ${ARG_PROJECT_NAME}"
+		COMMENT "Packaging ${FULL_PROJECT_NAME}"
 		)
-	set_property( TARGET ${ARG_PROJECT_NAME}-install PROPERTY FOLDER ${ARG_PROJECT_NAME} )
-
-	add_custom_target(
-		${ARG_PROJECT_NAME}-package
-		COMMAND ${BUILD_CMD} package
-			1>> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.log
-			2>> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.err
-		DEPENDS ${ARG_PROJECT_NAME}-install
-		WORKING_DIRECTORY ${BUILD_DIR}
-		COMMENT "Packaging ${ARG_PROJECT_NAME}"
-		)
-	set_property( TARGET ${ARG_PROJECT_NAME}-package PROPERTY FOLDER ${ARG_PROJECT_NAME} )
+	set_property( TARGET ${FULL_PROJECT_NAME}-package PROPERTY FOLDER ${FULL_PROJECT_NAME} )
 
 	if( UNIX )
 		# RDB: Thu Jun 26 13:45:46 PDT 2014
@@ -191,62 +188,62 @@ function( ConfigureMakeProject )
 				list( APPEND DPUT_DEPS     ${DEP}-dput     )
 			endforeach()
 			add_custom_target(
-				${ARG_PROJECT_NAME}-incdeps
-				COMMAND ${INC_DEPS_SCRIPT} ${CMAKE_SOURCE_DIR} ${ARG_PROJECT_NAME} ${ARG_DEPENDS}
+				${FULL_PROJECT_NAME}-incdeps
+				COMMAND ${INC_DEPS_SCRIPT} ${CMAKE_SOURCE_DIR} ${FULL_PROJECT_NAME} ${ARG_DEPENDS}
 				WORKING_DIRECTORY ${SRC_DIR}
-				COMMENT "Incrementing dependencies for debian package ${ARG_PROJECT_NAME}"
+				COMMENT "Incrementing dependencies for debian package ${FULL_PROJECT_NAME}"
 				)
 		else()
-			add_custom_target( ${ARG_PROJECT_NAME}-incdeps )
+			add_custom_target( ${FULL_PROJECT_NAME}-incdeps )
 		endif()
 		unset( NOINC_DEBVERS )
 		if( ARG_NOINC_DEBVERS )
 			set( NOINC_DEBVERS "--noinc" )
 		endif()
 		add_custom_target(
-			${ARG_PROJECT_NAME}-debuild
+			${FULL_PROJECT_NAME}-debuild
 			COMMAND env DEBEMAIL="${EMAIL_ADDY}" ${MAKE_SOURCE_SCRIPT} ${NOINC_DEBVERS} ${DEBUILD_PLATFORM}
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_debuild.log
+				1> ${BUILD_DIR}/${FULL_PROJECT_NAME}_debuild.log
 			WORKING_DIRECTORY ${SRC_DIR}
-			DEPENDS ${ARG_PROJECT_NAME}-incdeps
-			COMMENT "Building debian source package for ${ARG_PROJECT_NAME}"
+			DEPENDS ${FULL_PROJECT_NAME}-incdeps
+			COMMENT "Building debian source package for ${FULL_PROJECT_NAME}"
 			)
 		add_custom_target(
-			${ARG_PROJECT_NAME}-dput
+			${FULL_PROJECT_NAME}-dput
 			COMMAND ${MAKE_DPUT_SCRIPT}
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_dput.log
+				1> ${BUILD_DIR}/${FULL_PROJECT_NAME}_dput.log
 			DEPENDS ${DPUT_DEPS}
 			WORKING_DIRECTORY ${SRC_DIR}
-			COMMENT "Dputting debian package ${ARG_PROJECT_NAME} to launchpad."
+			COMMENT "Dputting debian package ${FULL_PROJECT_NAME} to launchpad."
 			)
 		add_custom_target(
-			${ARG_PROJECT_NAME}-pbuilder
+			${FULL_PROJECT_NAME}-pbuilder
 			COMMAND ${PBUILDER_SCRIPT} ${DEBUILD_PLATFORM} 
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_pbuilder.log
+				1> ${BUILD_DIR}/${FULL_PROJECT_NAME}_pbuilder.log
 			DEPENDS ${PBUILDER_DEPS}
 			WORKING_DIRECTORY ${SRC_DIR}
-			COMMENT "Building debian package ${ARG_PROJECT_NAME} with pbuilder-dist."
+			COMMENT "Building debian package ${FULL_PROJECT_NAME} with pbuilder-dist."
 			)
 	endif()
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}-clean
+		${FULL_PROJECT_NAME}-clean
 		COMMAND rm -rf ${RM_DIR}
 		)
-	set_property( TARGET ${ARG_PROJECT_NAME}-clean PROPERTY FOLDER ${ARG_PROJECT_NAME} )
+	set_property( TARGET ${FULL_PROJECT_NAME}-clean PROPERTY FOLDER ${FULL_PROJECT_NAME} )
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}
-		DEPENDS ${ARG_PROJECT_NAME}-package
+		${FULL_PROJECT_NAME}
+		DEPENDS ${FULL_PROJECT_NAME}-package
 		)
-	set_property( TARGET ${ARG_PROJECT_NAME} PROPERTY FOLDER ${ARG_PROJECT_NAME} )
+	set_property( TARGET ${FULL_PROJECT_NAME} PROPERTY FOLDER ${FULL_PROJECT_NAME} )
 
-	set_property( GLOBAL APPEND PROPERTY BUILD_TARGETS    ${ARG_PROJECT_NAME}          )
-	set_property( GLOBAL APPEND PROPERTY CLEAN_TARGETS    ${ARG_PROJECT_NAME}-clean    )
+	set_property( GLOBAL APPEND PROPERTY BUILD_TARGETS    ${FULL_PROJECT_NAME}          )
+	set_property( GLOBAL APPEND PROPERTY CLEAN_TARGETS    ${FULL_PROJECT_NAME}-clean    )
 	if( NOT MSVC )
-		set_property( GLOBAL APPEND PROPERTY PACKAGE_TARGETS  ${ARG_PROJECT_NAME}-debuild  )
-		set_property( GLOBAL APPEND PROPERTY DPUT_TARGETS     ${ARG_PROJECT_NAME}-dput     )
-		set_property( GLOBAL APPEND PROPERTY PBUILDER_TARGETS ${ARG_PROJECT_NAME}-pbuilder )
+		set_property( GLOBAL APPEND PROPERTY PACKAGE_TARGETS  ${FULL_PROJECT_NAME}-debuild  )
+		set_property( GLOBAL APPEND PROPERTY DPUT_TARGETS     ${FULL_PROJECT_NAME}-dput     )
+		set_property( GLOBAL APPEND PROPERTY PBUILDER_TARGETS ${FULL_PROJECT_NAME}-pbuilder )
 	endif()
 endfunction()
 
