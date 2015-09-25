m2osw/unigw

commit 371ca4301636144592dfa5d1f12d4fdf5d6c3d58

More logging, Windows improvements
I added more logging for when files are being manipulated. For example. when a file is copied.
Fixed problems with the environment vars WPKG_OUTPUT_PATH, etc, for the script hooks. They
needed to have OS-specific separators ('\'), otherwise scripts might fail.
Also, unlinking under windows can fail if permissions aren't fully read/write, so now
force them before unlinking.

wpkg/libdebpackages/wpkgar.cpp
@@ -2664,12 +2664,20 @@ bool wpkgar_manager::run_one_script(const wpkg_filename::uri_filename& package_n
     // of the package being installed, os, vendor, processor, etc.);
     // also we may want to reset our own environment to not leak stuff
     // that should not be visible to those scripts
-    const std::string env_root_path    ( std::string( "WPKG_ROOT_PATH="     ) += f_root_path     .os_filename().get_utf8() );
-    const std::string env_db_path      ( std::string( "WPKG_DATABASE_PATH=" ) += f_database_path .os_filename().get_utf8() );
-    const std::string env_package_name ( std::string( "WPKG_PACKAGE_NAME="  ) += package_name    .os_filename().get_utf8() );
+    std::string env_root_path    ( std::string( "WPKG_ROOT_PATH="     ) += f_root_path     .os_filename().get_utf8() );
+    std::string env_db_path      ( std::string( "WPKG_DATABASE_PATH=" ) += f_database_path .os_filename().get_utf8() );
+    std::string env_package_name ( std::string( "WPKG_PACKAGE_NAME="  ) += package_name    .os_filename().get_utf8() );
 
 #ifdef MO_WINDOWS
     int r;
+    //
+    // Since this is MSWindows, we have to make sure the
+    // slash is the "right" slash for the OS (e.g. '\', not '/').
+    //
+    std::replace( env_root_path.begin(),    env_root_path.end(),    '/', '\\' );
+    std::replace( env_db_path.begin(),      env_db_path.end(),      '/', '\\' );
+    std::replace( env_package_name.begin(), env_package_name.end(), '/', '\\' );
+    //
     r = _wputenv( libutf8::mbstowcs( env_root_path    ).c_str() );
     r = _wputenv( libutf8::mbstowcs( env_db_path      ).c_str() );
     r = _wputenv( libutf8::mbstowcs( env_package_name ).c_str() );
@@ -2685,9 +2693,9 @@ bool wpkgar_manager::run_one_script(const wpkg_filename::uri_filename& package_n
     fflush(stderr);
 #else
     int r;
-    r = putenv( const_cast<char*>( env_root_path    .c_str() ) );
-    r = putenv( const_cast<char*>( env_db_path      .c_str() ) );
-    r = putenv( const_cast<char*>( env_package_name .c_str() ) );
+    r = putenv( env_root_path    .c_str() ) );
+    r = putenv( env_db_path      .c_str() ) );
+    r = putenv( env_package_name .c_str() ) );
     r = system( cmd.c_str() );
 #endif
     wpkg_output::log("system() call returned %1")

wpkg/libdebpackages/memfile.cpp
@@ -46,6 +46,7 @@
 #include    "libdebpackages/wpkg_stream.h"
 #include    "libdebpackages/wpkgar_block.h"
 #include    "libdebpackages/case_insensitive_string.h"
+#include    "libdebpackages/wpkg_output.h"
 
 #ifdef debpackages_EXPORTS
 #define BZ_IMPORT 1
@@ -1407,6 +1408,11 @@ void memory_file::read_file(const wpkg_filename::uri_filename& filename, file_in
 
     f_filename = filename;
 
+    wpkg_output::log("Reading file '%1'.")
+            .quoted_arg(f_filename.original_filename())
+        .debug(wpkg_output::debug_flags::debug_detail_files)
+        .module(wpkg_output::module_repository);
+
     // WARNING: here the filename may NOT have been canonicalized
     std::string scheme(filename.path_scheme());
 
@@ -1733,6 +1739,11 @@ void memory_file::write_file(const wpkg_filename::uri_filename& filename, bool c
 
     // this assignment may not always be correct, but in most cases it should be fine
     const_cast<wpkg_filename::uri_filename&>(f_filename) = filename;
+
+    wpkg_output::log("Writing file '%1'.")
+            .quoted_arg(f_filename.original_filename())
+        .debug(wpkg_output::debug_flags::debug_detail_files)
+        .module(wpkg_output::module_repository);
 
     wpkg_stream::fstream file;
     file.create(filename);

wpkg/libdebpackages/wpkg_filename.cpp
@@ -3480,14 +3480,26 @@ bool uri_filename::os_unlink() const
 {
     bool result(true);
 
-    if(unlink(os_filename().get_os_string().c_str()) != 0)
+    const auto fname( os_filename().get_os_string() );
+#ifdef MO_WINDOWS
+    // Make the read-only file read-write.
+    //
+    if( _wchmod( fname.c_str(), _S_IREAD | _S_IWRITE ) == -1 )
+    {
+        if(errno != ENOENT)
+        {
+            throw wpkg_filename_exception_io("file \"" + f_original + "\" could not be made read/write!");
+        }
+    }
+#endif
+    if( unlink(fname.c_str()) != 0 )
     {
         result = false;
 
         // this is an error only if the file exists and cannot be deleted
         if(errno != ENOENT)
         {
-            throw wpkg_filename_exception_io("file \"" + f_original + "\" could not be removed");
+            throw wpkg_filename_exception_io("file \"" + f_original + "\" could not be removed!");
         }
     }
 

wpkg/libdebpackages/wpkgar_install.cpp
@@ -2088,6 +2088,12 @@ void wpkgar_install::read_repositories()
             {
                 if(!index_filename.exists())
                 {
+                    wpkg_output::log("Creating index file, since it does not exist in repository '%1'.")
+                            .quoted_arg(*it)
+                        .debug(wpkg_output::debug_flags::debug_detail_config)
+                        .module(wpkg_output::module_validate_installation)
+                        .package(index_filename);
+
                     // that's a direct filename but the index is missing,
                     // create it on the spot
                     wpkgar_repository repository(f_manager);
@@ -2100,6 +2106,12 @@ void wpkgar_install::read_repositories()
                 }
                 else
                 {
+                    wpkg_output::log("Reading index file from repository '%1'.")
+                            .quoted_arg(*it)
+                        .debug(wpkg_output::debug_flags::debug_detail_config)
+                        .module(wpkg_output::module_validate_installation)
+                        .package(index_filename);
+
                     // index exists, read it
                     compressed.read_file(index_filename);
                     compressed.decompress(index_file);
@@ -2112,6 +2124,12 @@ void wpkgar_install::read_repositories()
                 // fails we just ignore that entry
                 try
                 {
+                    wpkg_output::log("Reading index file from remote repository '%1'.")
+                            .quoted_arg(*it)
+                        .debug(wpkg_output::debug_flags::debug_detail_config)
+                        .module(wpkg_output::module_validate_installation)
+                        .package(index_filename);
+
                     compressed.read_file(index_filename);
                     compressed.decompress(index_file);
                 }

commit b07bf440108bdc82b1f247693bf024d1d2d07a8f

MH's patch to fix the broken build on Linux.
My bad for not checking the build on Linux after commit, but it was
late. That's my story, and I'm sticking to it. :) Tests under Windows
coming forthwith.

MH's words:
"Commit 371ca43 broke the build on Linux. The POSIX-confirm variant of
putenv(3) takes a char *. The aforementioned commit removed a const_cast
around std::string::c_str's result. This was a good change as it
uncovered another issue: the environment contained pointers to the
stack, which is not going to go well after the corresponding scope is
left (wpkgar_manager::run_one_script).

"While fixing the code I figured that using std::map reduces code
duplication. On Windows _wputenv_s can be used instead of composing the
“NAME=VALUE” argument for _wputenv. The patch is below. It builds on
Debian Jessie (GCC 4.9.2) and Microsoft Windows (Visual C++ 2015).

"Note that there are more occurences of the “putenv(const_cast<char
*="">(…)>” pattern. Those will have to be fixed separately."

wpkg/libdebpackages/wpkgar.cpp
@@ -2659,28 +2659,34 @@ bool wpkgar_manager::run_one_script(const wpkg_filename::uri_filename& package_n
         .package(package_name)
         .action("execute-script");
 
-    // Add some putenv() to give the running process some info about
-    // the packager (i.e. package name, version, root path, full name
-    // of the package being installed, os, vendor, processor, etc.);
-    // also we may want to reset our own environment to not leak stuff
-    // that should not be visible to those scripts
-    std::string env_root_path    ( std::string( "WPKG_ROOT_PATH="     ) += f_root_path     .os_filename().get_utf8() );
-    std::string env_db_path      ( std::string( "WPKG_DATABASE_PATH=" ) += f_database_path .os_filename().get_utf8() );
-    std::string env_package_name ( std::string( "WPKG_PACKAGE_NAME="  ) += package_name    .os_filename().get_utf8() );
+    // Set a number of environment variables to give the running process some
+    // info about the packager (i.e. package name, version, root path, full
+    // name of the package being installed, os, vendor, processor, etc.); also
+    // we may want to reset our own environment to not leak stuff that should
+    // not be visible to those scripts
+    //
+    typedef std::map<std::string, std::string> env_map_t;
+    env_map_t env;
+    //
+    env["WPKG_ROOT_PATH"]     = f_root_path.os_filename().get_utf8();
+    env["WPKG_DATABASE_PATH"] = f_database_path.os_filename().get_utf8();
+    env["WPKG_PACKAGE_NAME"]  = package_name.os_filename().get_utf8();
 
 #ifdef MO_WINDOWS
     int r;
-    //
-    // Since this is MSWindows, we have to make sure the
-    // slash is the "right" slash for the OS (e.g. '\', not '/').
-    //
-    std::replace( env_root_path.begin(),    env_root_path.end(),    '/', '\\' );
-    std::replace( env_db_path.begin(),      env_db_path.end(),      '/', '\\' );
-    std::replace( env_package_name.begin(), env_package_name.end(), '/', '\\' );
-    //
-    r = _wputenv( libutf8::mbstowcs( env_root_path    ).c_str() );
-    r = _wputenv( libutf8::mbstowcs( env_db_path      ).c_str() );
-    r = _wputenv( libutf8::mbstowcs( env_package_name ).c_str() );
+
+    for( env_map_t::const_iterator it = env.cbegin(); it != env.cend(); ++it )
+    {
+        std::string value (it->second);
+        //
+        // Since this is MSWindows, we have to make sure the
+        // slash is the "right" slash for the OS (e.g. '\', not '/').
+        //
+        std::replace(value.begin(), value.end(), '/', '\\');
+        //
+        r = _wputenv_s(libutf8::mbstowcs(it->first).c_str(),
+                       libutf8::mbstowcs(value).c_str());
+    }
 
     // under MS-Windows we have to convert the UTF-8 to UTF-16 before
     // calling the system function or it will fail if characters outside
@@ -2693,9 +2699,12 @@ bool wpkgar_manager::run_one_script(const wpkg_filename::uri_filename& package_n
     fflush(stderr);
 #else
     int r;
-    r = putenv( env_root_path    .c_str() ) );
-    r = putenv( env_db_path      .c_str() ) );
-    r = putenv( env_package_name .c_str() ) );
+
+    for( env_map_t::const_iterator it = env.cbegin(); it != env.cend(); ++it )
+    {
+        r = setenv( it->first.c_str(), it->second.c_str(), 1 );
+    }
+
     r = system( cmd.c_str() );
 #endif
     wpkg_output::log("system() call returned %1")